ed1bb0492b99430e5931eed7ad583f37be476ea3
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-01 20:21:42 -0500

    feat(welcome): added first and last name as properties.

--- src/app/welcoming/components/welcome/welcome.component.ts
+++ src/app/welcoming/components/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  @Input() firstName!: string;
+  @Input() lastName!: string;
 
   get fullName(): string {
     return 'Unknown';
