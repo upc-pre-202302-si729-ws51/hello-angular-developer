b31c92e3a0077aea71cfedf3417f09c813ab0020
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-01 19:59:12 -0500

    chore: initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular-developer';
+  title = 'Hello Angular Developer App';
 }
