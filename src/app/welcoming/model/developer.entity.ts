a59926c2b984648f79722f5b40adef207ec7ff9d
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-01 20:22:13 -0500

    chore(developer): added developer entity.

---
+++ src/app/welcoming/model/developer.entity.ts
@@ -0,0 +1,13 @@
+export class Developer {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
